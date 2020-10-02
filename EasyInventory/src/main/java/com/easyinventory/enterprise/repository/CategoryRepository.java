bb1f14fec422469f5bcf4b615da66d142982969e
ANagal5465 <53920484+ANagal5465@users.noreply.github.com>
2020-10-02 00:55:39 -0400

    First push
    This update contains code for the some of the backend API calls as well as the dependancies built. We planned on using a cloud DB but there were issues connecting so the local is what we have currently.

---
+++ EasyInventory/src/main/java/com/easyinventory/enterprise/repository/CategoryRepository.java
@@ -0,0 +1,22 @@
+package com.easyinventory.enterprise.repository;
+
+import com.easyinventory.enterprise.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static org.hibernate.loader.Loader.SELECT;
+
+
+/**
+ * Author: Anthony Nagal
+ *
+ * This is the repository for the Categories. It will talk to the database and hold the data
+ * in java persistence
+ */
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    //Ensures the categories are sorted correctly, future-proofing but most-likely redundant.
+    List<Category> findAllByOrderByCategoryIdAsc();
+}
---
+++ EasyInventory/src/main/java/com/easyinventory/enterprise/repository/ItemRepository.java
@@ -0,0 +1,22 @@
+package com.easyinventory.enterprise.repository;
+
+import com.easyinventory.enterprise.model.Category;
+import com.easyinventory.enterprise.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+/**
+ * Author: Anthony Nagal
+ *
+ * This is the repository for the items. It will talk to the database and hold the data
+ * in java persistence
+ */
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Integer> {
+    //This method will sort all the items by their categoryId so that when
+    //new items are added they will stll be grouped appropriately in the JSON
+    List<Item> findAllByOrderByCategoryIdAsc();
+}
